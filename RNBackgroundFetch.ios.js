889e204b58925c8a7b8ce940a156d1af60d90f7e
Chris Scott <christocracy@gmail.com>
2016-08-06 11:54:21 -0400

    Remove configured flag

--- RNBackgroundFetch.ios.js
+++ RNBackgroundFetch.ios.js
@@ -2,12 +2,7 @@ const { React, DeviceEventEmitter} = require('react-native');
 const { RNBackgroundFetch } = require('react-native').NativeModules;
 const TAG = "RNBackgroundFetch";
 var API = {
-  configured: false,
   configure: function(config, callback, failure) {
-    if (this.configured) {
-      console.warn(TAG, "has already been configured");
-      return;
-    }
     if (typeof(callback) !== 'function') {
       throw "RNBackgroundFetch requires a fetch callback at 2nd argument";
     }
