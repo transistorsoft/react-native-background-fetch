85523f4b5d6fb6b56ea13278e3fc630e3bde5857
ZauberNerd <zaubernerd@zaubernerd.de>
2017-05-30 16:24:36 +0200

    make sure unlink doesn't break if some values are undefined

--- scripts/postunlink.js
+++ scripts/postunlink.js
@@ -46,16 +46,18 @@ file.target = project.getFirstTarget().uuid;
 
 project.removeFromPbxBuildFileSection(file);
 project.removeFromPbxFileReferenceSection(file);
-project.removeFromFrameworksPbxGroup(file);
+if (project.pbxGroupByName('Frameworks')) {
+    project.removeFromFrameworksPbxGroup(file);
+}
 project.removeFromPbxFrameworksBuildPhase(file);
 
 helpers.removeFromFrameworkSearchPaths(
     project,
     '$(PROJECT_DIR)/' +
-        path.relative(
-            projectConfig.sourceDir,
-            path.join(moduleDirectory, 'ios')
-        )
+    path.relative(
+        projectConfig.sourceDir,
+        path.join(moduleDirectory, 'ios')
+    )
 );
 
 // remove AppDelegate extension
@@ -67,23 +69,26 @@ project.removeSourceFile(
 );
 
 // disable BackgroundModes and remove "fetch" mode from plist file
-const targetAttributes = helpers.getTargetAttributes(project)
-    .SystemCapabilities;
-delete targetAttributes['com.apple.BackgroundModes'].enabled;
-if (Object.keys(targetAttributes['com.apple.BackgroundModes']).length === 0) {
-    delete targetAttributes['com.apple.BackgroundModes'];
-}
-project.addTargetAttribute('SystemCapabilities', targetAttributes);
-if (Object.keys(targetAttributes).length === 0) {
-    project.removeTargetAttribute('SystemCapabilities');
+const systemCapabilities = helpers.getTargetAttributes(project).SystemCapabilities;
+if (systemCapabilities && systemCapabilities['com.apple.BackgroundModes']) {
+    delete systemCapabilities['com.apple.BackgroundModes'].enabled;
+    if (Object.keys(systemCapabilities['com.apple.BackgroundModes']).length === 0) {
+        delete systemCapabilities['com.apple.BackgroundModes'];
+    }
+
+    if (Object.keys(systemCapabilities).length === 0) {
+        project.removeTargetAttribute('SystemCapabilities');
+    }
 }
 
 const plist = helpers.readPlist(projectConfig.sourceDir, project);
-plist.UIBackgroundModes = plist.UIBackgroundModes.filter(
-    mode => mode !== 'fetch'
-);
-if (plist.UIBackgroundModes.length === 0) {
-    delete plist.UIBackgroundModes;
+if (Array.isArray(plist.UIBackgroundModes)) {
+    plist.UIBackgroundModes = plist.UIBackgroundModes.filter(
+        mode => mode !== 'fetch'
+    );
+    if (plist.UIBackgroundModes.length === 0) {
+        delete plist.UIBackgroundModes;
+    }
 }
 
 helpers.writePlist(projectConfig.sourceDir, project, plist);
