7bc51cb0cc4dfc43315c467c3592d27fbe55bdc6
MacGregor Thomson <macgregor@huffpost.com>
2019-05-23 14:39:53 -0400

    Wrap code in new thread in try

--- android/src/main/java/com/transistorsoft/rnbackgroundfetch/HeadlessTask.java
+++ android/src/main/java/com/transistorsoft/rnbackgroundfetch/HeadlessTask.java
@@ -97,7 +97,12 @@ public class HeadlessTask implements HeadlessJsTaskEventListener {
             UiThreadUtil.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    int taskId = headlessJsTaskContext.startTask(taskConfig);
+                    try {
+                        int taskId = headlessJsTaskContext.startTask(taskConfig);
+                    } catch (IllegalStateException exception) {
+                        Log.e(BackgroundFetch.TAG, "Headless task attempted to run in the foreground.  Task ignored.");
+                        return;  // <-- Do nothing.  Just return
+                    }
                 }
             });
         } catch (IllegalStateException exception) {
